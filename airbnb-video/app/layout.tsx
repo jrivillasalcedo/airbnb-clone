5b0e72512d2c4f489ee14f8354c1a46151eec8eb
jrivilla <jrivillarepository@gmail.com>
2023-04-29 19:11:08 +0200

    added toaster provider for register modal

--- airbnb-video/app/layout.tsx
+++ airbnb-video/app/layout.tsx
@@ -3,8 +3,8 @@ import { Nunito } from "next/font/google";
 import { Inter } from "next/font/google";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
-import Modal from "./components/modals/Modal";
 import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <ToasterProvider />
           <RegisterModal />
           <Navbar />
         </ClientOnly>
