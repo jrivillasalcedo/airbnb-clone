7fc1048e3068f04f8a3eb5cc7c6def736f18366c
jrivilla <jrivillarepository@gmail.com>
2023-05-01 19:13:41 +0200

    Fetching listings with server components (Listing card component, direct server action)

--- airbnb-video/app/types/index.ts
+++ airbnb-video/app/types/index.ts
@@ -1,4 +1,18 @@
-import { User } from "@prisma/client";
+import { Listing, Reservation, User } from "@prisma/client";
+
+export type SafeListing = Omit<Listing, "createdAt"> & {
+  createdAt: string;
+};
+
+export type SafeReservation = Omit<
+  Reservation,
+  "createdAt" | "startDate" | "endDate" | "listing"
+> & {
+  createdAt: string;
+  startDate: string;
+  endDate: string;
+  listing: SafeListing;
+};
 
 export type SafeUser = Omit<
   User,
@@ -6,5 +20,5 @@ export type SafeUser = Omit<
 > & {
   createdAt: string;
   updatedAt: string;
-  emailVerified: string;
+  emailVerified: string | null;
 };
